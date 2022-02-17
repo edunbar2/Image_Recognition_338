37e3312971c2b0ab9f83a76647ad6facb0f243e3
edunbar <edunbar@unca.edu>
2022-02-17 11:09:01 -0500

    Removed invalid citation

--- src/main.py
+++ src/main.py
@@ -2,16 +2,6 @@
 # and loading datasets for training
 # YouTube channel Edureka used for Neural Network information and training practices.
 # Dataset being used:
-# @Inproceedings (Conference){asirra-a-captcha-that-exploits-interest-aligned-manual-image-categorization,
-# author = {Elson, Jeremy and Douceur, John (JD) and Howell, Jon and Saul, Jared},
-# title = {Asirra: A CAPTCHA that Exploits Interest-Aligned Manual Image Categorization},
-# booktitle = {Proceedings of 14th ACM Conference on Computer and Communications Security (CCS)},
-# year = {2007},
-# month = {October},
-# publisher = {Association for Computing Machinery, Inc.},
-# url = {https://www.microsoft.com/en-us/research/publication/asirra-a-captcha-that-exploits-interest-aligned-manual-image-categorization/},
-# edition = {Proceedings of 14th ACM Conference on Computer and Communications Security (CCS)},
-# }
 
 import tensorflow as tf
 from tensorflow.keras import layers, models, datasets
